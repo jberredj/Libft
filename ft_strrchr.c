3fc9ce13c656ea0be82c62cc238c7607f269a08c
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-11-23 18:57:49 +0100

    Change in ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:55:16 by jberredj          #+#    #+#             */
-/*   Updated: 2020/11/23 13:02:20 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/11/23 18:57:36 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char	*ft_strrchr(const char *s, int c)
 	char	*last_char;
 	char	*curr_char;
 
-	last_char = NULL;
 	curr_char = ft_strchr(s, c);
+	last_char = curr_char;
 	while (curr_char != NULL)
 	{
 		curr_char = ft_strchr(curr_char + 1, c);
