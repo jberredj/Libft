617c8eaaa68e19963ff3c6c6e58f6b4afd69be4c
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-11-23 19:11:24 +0100

    Change to ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:11:36 by jberredj          #+#    #+#             */
-/*   Updated: 2020/11/23 16:55:46 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/11/23 19:11:15 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	uchar_s2 = (unsigned char*)s2;
 	while (n-- > 0)
 		if (*uchar_s1++ != *uchar_s2++)
-			return (*uchar_s1 - *uchar_s2);
+			return (*--uchar_s1 - *--uchar_s2);
 	return (0);
 }
