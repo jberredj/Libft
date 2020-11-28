641bcd5e9cb880bd7629de2463984ba74820ffbf
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-11-28 14:12:39 +0100

    Edit in strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 15:33:02 by jberredj          #+#    #+#             */
-/*   Updated: 2020/11/24 17:54:32 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/11/28 14:12:06 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i_len = len;
 	found = -1;
-	if (*haystack != '\0')
-	{
-		if (*needle == '\0')
-			return ((char*)haystack);
-		while (*haystack != '\0'
-		&& (found = ft_strncmp(haystack, needle, ft_strlen(needle))) != 0
-		&& len-- > 1)
-			haystack++;
-		if (i_len - len + ft_strlen(needle) > i_len)
-			return (NULL);
-		if (found == 0)
-			return ((char*)haystack);
-	}
+	if (*needle == '\0')
+		return ((char*)haystack);
+	while (*haystack != '\0'
+	&& (found = ft_strncmp(haystack, needle, ft_strlen(needle))) != 0
+	&& len-- > 1)
+		haystack++;
+	if (i_len - len + ft_strlen(needle) > i_len)
+		return (NULL);
+	if (found == 0)
+		return ((char*)haystack);
 	return (NULL);
 }
