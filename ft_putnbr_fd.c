6a076705063f7a0cb0b03089edb06eb545912980
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-11-30 20:26:46 +0100

    Update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:05:48 by jberredj          #+#    #+#             */
-/*   Updated: 2020/11/30 20:24:59 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/11/30 20:26:36 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	unsigned int	us_n;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		us_n = (unsigned int)n;
+	}
+	else 
+		us_n = (unsigned int)n;
+	if (us_n >= 10)
+	{
+		ft_putnbr_fd(us_n / 10, fd);
+		ft_putnbr_fd(us_n % 10, fd);
+	} 
+	else
+		ft_putchar_(n + '0', fd);
 }
