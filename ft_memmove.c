b41e268a5530a331a190ec426cb09e342310a888
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-11-30 21:14:45 +0100

    Update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 14:44:19 by jberredj          #+#    #+#             */
-/*   Updated: 2020/11/30 21:13:45 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/11/30 21:14:40 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	uchar_dest = (unsigned char*)dest;
 	uchar_src = (unsigned char*)src;
 	if (uchar_dest < uchar_src)
-		ft_memcpy(uchar_dest, uchar_src, n);
+		return (ft_memcpy(uchar_dest, uchar_src, n));
 	else
 		while (n--)
 			uchar_dest[n] = uchar_src[n];
