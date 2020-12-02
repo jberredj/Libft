46526a887ac01e9de254c8afbe5e33bf4438ca21
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-02 22:51:43 +0100

    Update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 10:06:25 by jberredj          #+#    #+#             */
-/*   Updated: 2020/12/02 22:51:19 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/12/02 22:51:40 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			if ((new_elem->content = f(lst->content)) == NULL)
 				return (ft_lstmap_clean(&new_list, del));
 			if ((new_elem->next = ft_lstnew(NULL)) == NULL)
-				return (ft_lstmap_clean(&new_list, desl));
+				return (ft_lstmap_clean(&new_list, del));
 			lst = lst->next;
 			new_elem = new_elem->next;
 		}
