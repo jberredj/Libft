ba254f48b9c4c4e8c3d7c991b0af026875085dba
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-02 13:25:23 +0100

    Update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 10:06:25 by jberredj          #+#    #+#             */
-/*   Updated: 2020/12/02 11:44:01 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/12/02 13:25:21 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_list;
 	t_list	*new_elem;
 
-	new_list = NULL;
-	new_elem = NULL;
 	if (lst != NULL)
 	{
-		if ((new_list = ft_lstnew(lst->content)) == NULL)
-		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		new_list = (*f)(new_list);
-		ft_lstadd_back(&new_list, new_elem);
+		new_list = ft_lstnew((*f)(lst->context));
 		lst = lst->next;
-	}
-	while (lst != NULL)
-	{
-		if ((new_elem = ft_lstnew(lst->content)) == NULL)
+		while (lst != NULL)
 		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
+			new_elem = ft_lstnew((*f)(lst->context));
+			ft_lstadd_back(&new_list, new_elem);
+			lst = lst->next;
 		}
-		new_elem = f(new_elem);
-		ft_lstadd_back(&new_list, new_elem);
 	}
-	return(new_list);
+	return (new_list);
 }
