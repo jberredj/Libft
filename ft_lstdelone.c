e5c44b019c784e22ec366537f546514d94873769
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-02 16:47:33 +0100

    Update

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 09:51:17 by jberredj          #+#    #+#             */
-/*   Updated: 2020/12/02 16:45:37 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/12/02 16:47:31 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 		del(lst->content);
 	}
 	free(lst);
+	lst = NULL;
 }
