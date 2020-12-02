c7374a8fa6aaf61c949480d015ee90953cbe5791
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-02 19:43:58 +0100

    Aled

--- libft_bonus.h
+++ libft_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 08:58:07 by jberredj          #+#    #+#             */
-/*   Updated: 2020/12/02 10:07:16 by jberredj         ###   ########.fr       */
+/*   Updated: 2020/12/02 19:18:51 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 # include <stdlib.h>
 # include "libft.h"
 
-typedef struct	s_list
+typedef struct		s_list
 {
-	void 		*content;
-	struct 		s_list *next;
-}				t_list;
-t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **alst, t_list *new);
-int				ft_lstsize(t_list *lst);
-t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **alst, t_list *new);
-void			ft_lstdelone(t_list *lst, void (*del)(void*));
-void			ft_lstclear(t_list **lst, void (*del)(void*));
-void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **alst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **alst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void*));
+void				ft_lstclear(t_list **lst, void (*del)(void*));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 #endif
