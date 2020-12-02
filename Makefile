36685b87eb4bf03ed4e2347ad0bd508cf3f13bf2
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2020-12-02 09:35:00 +0100

    Add lstsize

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ SRCS		=	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c 
 				ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c \
 				ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 
-SRCS_BONUS	=	ft_lstnew.c ft_lstadd_front.c
+SRCS_BONUS	=	ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c
 
 OBJS_BONUS	=	${SRCS_BONUS:.c=.o}
 
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/02 08:58:07 by jberredj          #+#    #+#             */
-/*   Updated: 2020/12/02 09:34:13 by jberredj         ###   ########.fr       */
+/*   Created: 2020/12/02 09:29:03 by jberredj          #+#    #+#             */
+/*   Updated: 2020/12/02 09:33:07 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
-# include <stdlib.h>
-# include "libft.h"
+#include "libft_bonus.h"
 
-typedef struct	s_list
+int	ft_lstsize(t_list *lst)
 {
-	void 		*content;
-	struct 		s_list *next;
-}				t_list;
-t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **alst, t_list *new);
-int				ft_lstsize(t_list *lst);
-t_list			*ft_lstlast(t_list *lst);
-#endif
+	int size;
+
+	size = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		size++;
+	}
+	return (size);
+}
